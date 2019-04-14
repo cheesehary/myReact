34489b14bb7e9ecefdff3a7d96a7f284aaafed7b
admin <malin89218@163.com>
2019-04-14 17:59:00 +0800

    update

--- src/Component.ts
+++ src/Component.ts
@@ -29,6 +29,7 @@ export abstract class ReactComponent {
     this._curElement = reactEl;
   }
   abstract mountComponent(): HTMLElement;
+  abstract receiveComponent(nextEl: ReactElement): void;
 }
 
 export class ReactDOMComponent extends ReactComponent {
@@ -40,29 +41,59 @@ export class ReactDOMComponent extends ReactComponent {
     const element = this._curElement;
     const node = document.createElement(element.type as string);
     this._hostNode = node;
-    this.updateDOMProps(node, element.props);
+    this.updateDOMProps(null, element.props);
     const children: Array<ChildrenProp> = element.props.children;
-    this.appendChildrenNodes(node, children);
+    this.appendChildrenNodes(children);
     return node;
   }
 
-  updateDOMProps(node: HTMLElement, props) {
-    for (let propKey in props) {
-      if (ReservedProps.hasOwnProperty(propKey)) {
+  receiveComponent(nextEl: ReactElement) {
+    const prevEl = this._curElement;
+    this.updateDOMProps(prevEl.props, nextEl.props);
+  }
+
+  updateDOMProps(prevProps, nextProps) {
+    const node = this._hostNode;
+    for (let propKey in prevProps) {
+      if (
+        nextProps.hasOwnProperty(propKey) ||
+        !prevProps.hasOwnProperty(propKey) ||
+        prevProps[propKey] == null
+      ) {
         continue;
       }
       if (
         ListenerProps.hasOwnProperty(propKey) &&
-        typeof props[propKey] === "function"
+        typeof prevProps[propKey] === "function"
       ) {
-        node.addEventListener(ListenerProps[propKey], props[propKey]);
+        node.removeEventListener(ListenerProps[propKey], prevProps[propKey]);
       } else {
-        node.setAttribute(propKey, props[propKey]);
+        node.removeAttribute(propKey);
+      }
+    }
+    for (let propKey in nextProps) {
+      if (
+        ReservedProps.hasOwnProperty(propKey) ||
+        !nextProps.hasOwnProperty(propKey)
+      ) {
+        continue;
+      }
+      if (
+        ListenerProps.hasOwnProperty(propKey) &&
+        typeof nextProps[propKey] === "function"
+      ) {
+        node.addEventListener(ListenerProps[propKey], nextProps[propKey]);
+        if (prevProps[propKey]) {
+          node.removeEventListener(ListenerProps[propKey], prevProps[propKey]);
+        }
+      } else {
+        node.setAttribute(propKey, nextProps[propKey]);
       }
     }
   }
 
-  appendChildrenNodes(node: HTMLElement, children: Array<ChildrenProp>) {
+  appendChildrenNodes(children: Array<ChildrenProp>) {
+    const node = this._hostNode;
     if (children.length) {
       children.forEach(child => {
         if (typeof child === "string" || typeof child === "number") {
@@ -80,6 +111,7 @@ export class ReactDOMComponent extends ReactComponent {
 
 export class ReactClassComponent extends ReactComponent {
   public _instance: Component;
+  public _renderedComponent: ReactComponent;
   public _pendingState;
 
   constructor(reactEl: ReactElement) {
@@ -95,21 +127,42 @@ export class ReactClassComponent extends ReactComponent {
     ReactInstanceMap.set(inst, this);
     const renderedEl = inst.render();
     const childComponent = this._instantiateComponent(renderedEl);
+    this._renderedComponent = childComponent;
     return childComponent.mountComponent();
   }
 
   updateComponent(prevEl: ReactElement, nextEl: ReactElement) {
     const inst = this._instance;
     const nextState = this.processPendingState();
+    this._curElement = nextEl;
     inst.state = nextState;
     inst.props = nextEl.props;
-    if(onlyUpdateComponent(prevEl, nextEl)) {
-
+    const prevRenderedComponent = this._renderedComponent;
+    const prevRenderedEl = prevRenderedComponent._curElement;
+    const nextRenderedEl = inst.render();
+    if (onlyUpdateComponent(prevRenderedEl, nextRenderedEl)) {
+      prevRenderedComponent.receiveComponent(nextRenderedEl);
     }
+  }
+
+  receiveComponent(nextEl: ReactElement) {
+    const prevEl = this._curElement;
+    this.updateComponent(prevEl, nextEl);
+  }
+
+  processPendingState() {
+    const inst = this._instance;
+    const partialState = this._pendingState;
+    if (!partialState) {
+      return inst.state;
+    }
+    return Object.assign({}, inst.state, partialState);
   }
 }
 
 export class ReactFunctionalComponent extends ReactComponent {
+  public _renderedComponent: ReactComponent;
+
   constructor(reactEl: ReactElement) {
     super(reactEl);
   }
@@ -121,4 +174,28 @@ export class ReactFunctionalComponent extends ReactComponent {
     const childComponent = this._instantiateComponent(renderedEl);
     return childComponent.mountComponent();
   }
+
+  receiveComponent(nextEl: ReactElement) {
+    const prevEl = this._curElement;
+    this._curElement = nextEl;
+    if (onlyUpdateComponent(prevEl, nextEl)) {
+      const prevRenderedComponent = this._renderedComponent;
+      const type = nextEl.type as SFC;
+      const nextRenderedEl = type(nextEl.props);
+      prevRenderedComponent.receiveComponent(nextRenderedEl);
+    }
+  }
+}
+
+function onlyUpdateComponent(
+  prevEl: ReactElement,
+  nextEl: ReactElement
+): boolean {
+  if (!prevEl && !nextEl) {
+    return true;
+  }
+  if (!prevEl || !nextEl) {
+    return false;
+  }
+  return nextEl.type === prevEl.type;
 }
